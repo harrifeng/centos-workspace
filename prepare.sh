e01964a6bccf2ee44c5b0bbb766f9b6ce55747a4
Harri Feng <harrifeng@gmail.com>
2017-09-23 20:23:57 +0800

    jdk

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 sudo yum install -y epel-release
-sudo yum install -y python-devel python-pip gcc git tmux
+sudo yum install -y python-devel python-pip gcc git tmux java-1.8.0-openjdk
 sudo pip install -U pip -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
 sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
 echo "<----------finishing provision--------------->"
