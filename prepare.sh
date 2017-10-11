4ec32e598b441d2c092413b434ef50da61ffaa6e
Harri Feng <harrifeng@gmail.com>
2017-10-11 15:58:46 +0800

    add the dns setting

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,6 @@ sudo yum install -y epel-release
 sudo yum install -y python-devel python-pip gcc git tmux java-1.8.0-openjdk the_silver_searcher
 sudo pip install -U pip -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
 sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
+
+echo "DNS1=8.8.8.8" | sudo tee -a /etc/sysconfig/network-scripts/ifcfg-enp0s3
 echo "<----------finishing provision--------------->"
