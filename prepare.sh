cb3ffa9212d22b1f2cbec585e0b1b77aca687bb8
fenghaoran <harrifeng@gmail.com>
2017-11-27 14:09:25 +0800

    new prepare

--- prepare.sh
+++ prepare.sh
@@ -3,18 +3,13 @@ sudo yum install -y lrzsz python-devel python-pip gcc git tmux java-1.8.0-openjd
 sudo pip install -U pip -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
 sudo pip install ansible virtualenv -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
 
+rm -rf /home/vagrant/github
 mkdir -p /home/vagrant/github
 cd /home/vagrant/github
 git clone https://github.com/harrifeng/dotfiles.git
-ln -s /home/vagrant/dotfiles/tmux/dot_tmux.conf /home/vagrant/.tmux.conf
-ln -s /home/vagarnt/dotfiles/git/dot_gitconfig /home/vagrant/.gitconfig
-ln -s /home/vagarnt/dotfiles/ansible/dot_ansible.cfg /home/vagrant/.ansible.cfg
-ln -s /home/vagrant/dotfiles/vagrant/dot_ssh/id_rsa.pub /home/vagrant/.ssh/id_rsa.pub
-
-git clone https://github.com/harrifeng/ansible-playground.git
-
-cd /home/vagrant/github/ansible-playground/04-install-new-git-on-centos
-echo 'vagrant' | bash run.sh
+ln -sf /home/vagrant/github/dotfiles/tmux/dot_tmux.conf /home/vagrant/.tmux.conf
+ln -sf /home/vagarnt/github/dotfiles/git/dot_gitconfig /home/vagrant/.gitconfig
+ln -sf /home/vagarnt/github/dotfiles/ansible/dot_ansible.cfg /home/vagrant/.ansible.cfg
 
 echo "DNS1=8.8.8.8" | sudo tee -a /etc/sysconfig/network-scripts/ifcfg-enp0s3
 echo "<----------finishing provision--------------->"
