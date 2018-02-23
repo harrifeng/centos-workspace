ec80f8a88a24bc66330ee7e82d09c29bffaaed59
Haoran Feng <harrifeng@gmail.com>
2018-02-23 19:24:20 +0800

    improve

--- prepare.sh
+++ prepare.sh
@@ -1,28 +1,28 @@
-# sudo yum install -y epel-release
-# sudo yum install -y lrzsz python-devel python-pip gcc git tmux java-1.8.0-openjdk the_silver_searcher htop
-# sudo pip install -U pip -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
-# sudo pip install ansible virtualenv -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
-#
-# echo "DNS1=8.8.8.8" | sudo tee -a /etc/sysconfig/network-scripts/ifcfg-enp0s3
-# mkdir -p /home/vagrant/github
-# ANSIBLE_UTIL='/home/vagrant/github/ansible-playground'
-#
-# if [ ! -d "$ANSIBLE_UTIL" ]; then
-#     git clone https://github.com/harrifeng/ansible-playground.git ${ANSIBLE_UTIL}
-# fi
-#
-# declare -a arr=(00-install-common 03-install-docker-on-centos 04-install-new-git-on-centos \
-#                                   05-install-new-emacs-on-centos \
-#                                   06-install-supervisord-and-configure-systemd-on-centos \
-#                                   07-install-nodejs-on-centos \
-#                                   08-install-python3-on-centos \
-#                                   10-install-shellcheck-on-centos \
-#                                   12-install-golang-1-9-on-centos)
-#
-# for dir in "${arr[@]}"
-# do
-#   ansible-playbook -i ${ANSIBLE_UTIL}/etc_ansible_hosts ${ANSIBLE_UTIL}/${dir}/main.yml --extra-vars "variable_host=localhost ansible_become_pass=vagrant"
-# done
+sudo yum install -y epel-release
+sudo yum install -y lrzsz python-devel python-pip gcc git tmux java-1.8.0-openjdk the_silver_searcher htop
+sudo pip install -U pip -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
+sudo pip install ansible virtualenv -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
+
+echo "DNS1=8.8.8.8" | sudo tee -a /etc/sysconfig/network-scripts/ifcfg-enp0s3
+mkdir -p /home/vagrant/github
+ANSIBLE_UTIL='/home/vagrant/github/ansible-playground'
+
+if [ ! -d "$ANSIBLE_UTIL" ]; then
+    git clone https://github.com/harrifeng/ansible-playground.git ${ANSIBLE_UTIL}
+fi
+
+declare -a arr=(00-install-common 03-install-docker-on-centos 04-install-new-git-on-centos \
+                                  05-install-new-emacs-on-centos \
+                                  06-install-supervisord-and-configure-systemd-on-centos \
+                                  07-install-nodejs-on-centos \
+                                  08-install-python3-on-centos \
+                                  10-install-shellcheck-on-centos \
+                                  12-install-golang-1-9-on-centos)
+
+for dir in "${arr[@]}"
+do
+  ansible-playbook -i ${ANSIBLE_UTIL}/etc_ansible_hosts ${ANSIBLE_UTIL}/${dir}/main.yml --extra-vars "variable_host=localhost ansible_become_pass=vagrant"
+done
 
 VIRTUAL_3ENV='/home/vagrant/venv/3ENV'
 
