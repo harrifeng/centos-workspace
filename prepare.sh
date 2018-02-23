bd621e4a62666bdfe1732a245db7b23224955d5f
Haoran Feng <harrifeng@gmail.com>
2018-02-23 10:41:38 +0800

    workable edition

--- prepare.sh
+++ prepare.sh
@@ -4,4 +4,12 @@ sudo pip install -U pip -i http://mirrors.aliyun.com/pypi/simple --trusted-host=
 sudo pip install ansible virtualenv -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
 
 echo "DNS1=8.8.8.8" | sudo tee -a /etc/sysconfig/network-scripts/ifcfg-enp0s3
+mkdir -p /home/vagrant/github
+ANSIBLE_UTIL='/home/vagrant/github/ansible-playground'
+
+if [ ! -d "$ANSIBLE_UTIL" ]; then
+    git clone https://github.com/harrifeng/ansible-playground.git /home/vagrant/github/ansible-playground
+fi
+
+ansible-playbook -i ${ANSIBLE_UTIL}/etc_ansible_hosts ${ANSIBLE_UTIL}/00-install-common/common.yml --extra-vars "variable_host=localhost ansible_become_pass=vagrant"
 echo "<----------finishing provision--------------->"
