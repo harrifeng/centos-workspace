a9b30d204fe2259db98d41e8e70434c86272feb8
Haoran Feng <harrifeng@gmail.com>
2018-08-11 10:09:58 +0800

    dotfiles at last

--- prepare.sh
+++ prepare.sh
@@ -7,10 +7,12 @@ echo "DNS1=8.8.8.8" | sudo tee -a /etc/sysconfig/network-scripts/ifcfg-enp0s3
 mkdir -p /home/vagrant/github
 mkdir -p /home/vagrant/venv
 
-DOT_FILES='/home/vagrant/github/dotfiles'
-if [ ! -d "$DOT_FILES" ]; then
-    git clone -b vagrant https://github.com/harrifeng/dotfiles.git ${DOT_FILES}
-    bash ${DOT_FILES}/run.sh
+
+ANSIBLE_UTIL='/home/vagrant/github/ansible-playground'
+if [ ! -d "$ANSIBLE_UTIL" ]; then
+    git clone -b vagrant https://github.com/harrifeng/ansible-playground.git ${ANSIBLE_UTIL}
+    PASSWD=vagrant
+    bash ${ANSIBLE_UTIL}/install.sh
 fi
 
 UTIL_BINARY='/home/vagrant/github/binary'
@@ -19,12 +21,12 @@ if [ ! -d "$UTIL_BINARY" ]; then
     bash ${UTIL_BINARY}/amd64/dot-cow/prepare.sh
 fi
 
-ANSIBLE_UTIL='/home/vagrant/github/ansible-playground'
-if [ ! -d "$ANSIBLE_UTIL" ]; then
-    git clone -b vagrant https://github.com/harrifeng/ansible-playground.git ${ANSIBLE_UTIL}
-    PASSWD=vagrant
-    bash ${ANSIBLE_UTIL}/install.sh
+DOT_FILES='/home/vagrant/github/dotfiles'
+if [ ! -d "$DOT_FILES" ]; then
+    git clone -b vagrant https://github.com/harrifeng/dotfiles.git ${DOT_FILES}
+    bash ${DOT_FILES}/run.sh
 fi
+
 
 chown -R vagrant:vagrant /home/vagrant/github
 echo "<----------finishing provision--------------->"
