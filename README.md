f2cf1bbe465e57a23800243598d2f1608a71caa9
Harri Feng <harrifeng@gmail.com>
2018-08-27 13:56:37 +0800

    Simple the setting

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # centos-workspace
 centos vagrant on all platform
+## Download box file and set by hand
+
+```
+vagrant box add "bento/centos-7.3" ./virtualbox.box
+vagrant box list
+```
--- prepare.sh
+++ prepare.sh
@@ -5,28 +5,12 @@ sudo pip install ansible virtualenv -i http://mirrors.aliyun.com/pypi/simple --t
 
 echo "DNS1=8.8.8.8" | sudo tee -a /etc/sysconfig/network-scripts/ifcfg-enp0s3
 mkdir -p /home/vagrant/github
-mkdir -p /home/vagrant/venv
-
-
-ANSIBLE_UTIL='/home/vagrant/github/ansible-playground'
-if [ ! -d "$ANSIBLE_UTIL" ]; then
-    git clone -b vagrant https://github.com/harrifeng/ansible-playground.git ${ANSIBLE_UTIL}
-    PASSWD=vagrant
-    bash ${ANSIBLE_UTIL}/install.sh
-fi
-
-UTIL_BINARY='/home/vagrant/github/binary'
-if [ ! -d "$UTIL_BINARY" ]; then
-    git clone -b vagrant https://github.com/harrifeng/binary.git ${UTIL_BINARY}
-    bash ${UTIL_BINARY}/amd64/dot-cow/prepare.sh
-fi
 
 DOT_FILES='/home/vagrant/github/dotfiles'
 if [ ! -d "$DOT_FILES" ]; then
-    git clone -b vagrant https://github.com/harrifeng/dotfiles.git ${DOT_FILES}
+    git clone https://github.com/harrifeng/dotfiles.git ${DOT_FILES}
     bash ${DOT_FILES}/run.sh
 fi
-
 
 chown -R vagrant:vagrant /home/vagrant/github
 echo "<----------finishing provision--------------->"
